d42d9b6b3bcbab3953bd95cced80f28114853019
tony turner <tony.turner@carbonplace.com>
2024-05-11 17:20:19 +0100

    added new graphic

--- src/components/hero-banner/hero-area-2.js
+++ src/components/hero-banner/hero-area-2.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react"
-import video from '../../../public/assets/img/retrofit/37577-414754938.mp4'
+import video from '../../../public/assets/img/retrofit/1068696866-preview.mp4'
 
 const HeroAreaTwo = () => {
   const videoRef = useRef(null)
@@ -10,57 +10,21 @@ const HeroAreaTwo = () => {
   }, [])
 
   return (
-    <section className="banner-area banner-style-two position-relative" style={{ minHeight: '120vh', alignItems: 'start', background: '#c2f2ff' }}>
-      <div className="row justify-content-center">
-        <div className="col-lg-12 d-none d-lg-block">
-        <div className="banner__background-wrap" style={{ minHeight: '102vh', marginTop: '20%', zIndex: 1000 }}>
-          <div
-            className="background"
-            //style={{ backgroundImage: "url(/assets/img/retrofit/streetscape.jpg)", opacity: 1.0 }}
-            style={{ opacity: 0.75 }}
-          >
-            <video src={video} autoPlay loop ref={videoRef} muted />
-          </div>
-        </div>
-      </div>
-      </div>
-
+    <section className="banner-area banner-style-two" style={{ background: '#FFF' }}>
       <div className="container">
-        <div>
-          <div
-            className="banner__images-two"
-          >
-            {/*<img
-              className="shape-two"
-              width="64"
-              src="/assets/img/objects/wind-turbine-6.svg"
-              alt="object"
-              style={{ top: '50%', left: '90%' }}
-              data-anime="opacity:[0, 1]; scale:[0, 1]; onview: true; delay: 400;"
-  />*/}
-            {/*<img
-              className="shape-five"
-              width="64"
-              src="/assets/img/objects/wind-turbine-6.svg"
-              alt="object"
-              style={{ top: '-80px', left: '70%' }}
-              data-anime="opacity:[0, 1]; scale:[0, 1]; onview: true; delay: 400;"
-  />*/}
+        <div className="row">
+          <div className="col-xl-6 col-lg-6">
+            <video src={video} autoPlay loop ref={videoRef} muted style={{
+              width: '100%',
+              height: '100%',
+            }} />
           </div>
-        </div>
-        <div className="row justify-content-center" style={{ marginTop: '8%' }}>
-          <div className="col-xl-7 col-lg-9">
-
-            <div
-              className="banner__content-two"
-            >
-            </div>
-
-            <h2 className="title" style={{ fontSize: 64, lineHeight: 1, zIndex: 3000}}><a href="#netZero">Net Zero</a> <span className="tg-text-gradient">Heroes</span></h2>
-            <div className="container" style={{zIndex: 1001}}>
-              <div className="row">
+          <div className="col-xl-6 col-lg-6">
+            <h2 className="title" style={{ fontSize: 64, lineHeight: 1 }}><a href="#netZero">Net Zero</a> <span className="tg-text-gradient">Heroes</span></h2>
+            <div className="container">
+              <div className="row justify-content-center">
                 <div className="col-lg-12" style={{ fontSize: 24, color: '#000', marginTop: 10 }}>
-                  <p style={{ fontSize: 24, marginBottom: 10 }}>Supporting the real heroes fighting climate change with the data and technology to make a difference.</p>
+                  <p style={{ fontSize: 24, marginBottom: 10, color: '#000' }}>Supporting the real heroes fighting climate change with the data and technology to make a difference.</p>
                   people <img src="/assets/img/retrofit/icons8-sum-50.png" />
                   data <img src="/assets/img/retrofit/icons8-sum-50.png" />
                   technology
@@ -75,9 +39,15 @@ const HeroAreaTwo = () => {
                 </a>
               </div>
             </div>
+
           </div>
         </div>
       </div>
+
+
+
+
+
     </section>
   )
 }
