3416108c91ba222b7a3e5bcd83066467b4d1e7dc
tony turner <tony.turner@carbonplace.com>
2024-05-13 13:30:56 +0100

    added new graphic

--- src/components/hero-banner/hero-area-2.js
+++ src/components/hero-banner/hero-area-2.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from "react"
 import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import video from '../../../public/assets/img/retrofit/1096048299-preview.mp4'
+import video from '../../../public/assets/img/retrofit/shutterstock_1096048299.mp4'
 import video2 from '../../../public/assets/img/retrofit/1068696866-preview.mp4'
 
 const HeroAreaTwo = () => {
@@ -21,44 +21,46 @@ const HeroAreaTwo = () => {
     dots: true,
     infinite: true,
     //autoplay: true,
-    speed: 500,
+    speed: 1500,
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    arrows: false
   }
 
   return (
     <div>
-      <section className="banner-area banner-style-two">
-        <div className="banner__background-wrap" style={{ minHeight: '102vh' }}>
+      <section className="banner-area banner-style-two" style={{ alignItems: 'start' }}>
+        <div className="banner__background-wrap" style={{ minHeight: '10vh' }}>
           <div
-            className="background"
           //style={{ backgroundImage: "url(/assets/img/retrofit/streetscape.jpg)", opacity: 0.1 }}
           >
-            <div className="container" style={{maxWidth: '100%'}}>
+            <div className="container" style={{ maxWidth: '2000px' }}>
               <div className="row">
                 <div className="col-xl-12 col-lg-12 d-none d-lg-block">
-                  <div>
+                  <div >
                     <video src={video} autoPlay ref={videoRef} muted style={{
                       width: '100%',
                       height: '100%',
-                      opacity: '1.0'
+                      opacity: '0.2',
+                      marginTop: '100px'
                     }} />
                   </div>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
         <div className="container">
           <div className="row">
             <div className="col-xl-6 col-lg-6">
-              <h2 className="title" style={{ fontSize: 64, lineHeight: 1 }}><a href="#netZero">Net Zero</a> <span className="tg-text-gradient">Heroes</span></h2>
+              <h2 className="title" style={{ fontSize: 64, lineHeight: 1, marginTop: '20%' }}><a href="#netZero">Net Zero</a> <span className="tg-text-gradient">Heroes</span></h2>
               <div className="container">
                 <div className="row justify-content-center">
-                  <div className="col-lg-12" style={{ fontSize: 24, color: '#000', marginTop: 10 }}>
-                    <p style={{ fontSize: 24, marginBottom: 10, color: '#000' }}>Supporting the real heroes fighting climate change with the data and technology to make a difference.</p>
+                  <div className="col-lg-12" style={{ fontSize: 24, color: '#000' }}>
+                    <p style={{ fontSize: 24, marginBottom: 10, color: '#000' }}>Communities in partnership with councils, Government and the private sector.</p>
                     <a
-                      href="https://opensea.io/"
+                      href="#people-data"
                       className="banner__btn btn gradient-btn gradient-btn-2 scroll-to-target"
                     >
                       <span>People + Data + Technology</span>
@@ -69,29 +71,18 @@ const HeroAreaTwo = () => {
               </div>
             </div>
             <div className="col-xl-6 col-lg-6">
-              <Slider {...settings}>
-                <div>
-                  <video src={video2} autoPlay loop ref={videoRef2} muted style={{
-                    width: '100%',
-                    //height: '100%',
-                  }} />
-                  <div>Learn how people + data + technology =</div>
-                </div>
+              <div className="row ml-auto p-2" style={{ marginTop: '20%', padding: '100px' }}>
+                <Slider {...settings}>
+                  <div style={{ fontSize: 24, color: '#000' }}>
+                    <a href="https://climateemergency.uk/wp-content/uploads/2024/03/Scorecards_Successes_report_CEUK_DIGITAL.pdf" target="_blank">
+                      <img src="/assets/img/retrofit/report-image-edited.jpg" width="75%" />
+                    </a>
+                  </div>
 
-                <div>
-                </div>
-                <div></div>
-              </Slider>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-12 col-lg-12">
-              <div>
-                kpokpppo
+                  <div>
+                  </div>
+                  <div></div>
+                </Slider>
               </div>
             </div>
           </div>
